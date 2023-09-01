ef000b00bcb71c64be2f954b8c47241019bf286d
ritobrotos <rito.rcks@gmail.com>
2023-09-01 11:31:33 +0530

    commit faiss_db utility

---
+++ vector_store_utilities/faiss_db_utility.py
@@ -0,0 +1,12 @@
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import FAISS
+from langchain.embeddings.openai import OpenAIEmbeddings
+
+
+def store_embeddings(content_list):
+    text_splitter = RecursiveCharacterTextSplitter(separators=["\n\n", "\n"], chunk_size=1000, chunk_overlap=150)
+    docs = text_splitter.create_documents(content_list)
+
+    embeddings = OpenAIEmbeddings()
+    faiss_db = FAISS.from_documents(docs, embeddings)
+    return faiss_db
