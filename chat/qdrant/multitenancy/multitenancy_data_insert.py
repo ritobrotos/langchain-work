2e295227b8566bbbe819af79a2225842da4eff31
ritobrotos <rito.rcks@gmail.com>
2024-01-27 12:08:33 +0530

    Updated Multitenancy data insert file

--- chat/qdrant/multitenancy/multitenancy_data_insert.py
+++ chat/qdrant/multitenancy/multitenancy_data_insert.py
@@ -2,19 +2,23 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.document_loaders import TextLoader
 from langchain_community.vectorstores import Qdrant
 from langchain_openai import OpenAIEmbeddings
+
+from chat.qdrant.multitenancy.multitenancy_constants import VECTOR_DB_COLLECTION, HISTORY_DEPARTMENT_NAME, \
+    SCIENCE_DEPARTMENT_NAME
 from constants import QDRANT_URL, QDRANT_API_KEY
 
 
-embeddings = OpenAIEmbeddings()
+
+
+
 CHUNK_SIZE = 1500
 CHUNK_OVERLAP = 300
-VECTOR_DB_COLLECTION = "department_data"
-
 
 def chunk_data(file_name):
     loader = TextLoader(file_name)
     documents = loader.load()
-    text_splitter = CharacterTextSplitter(separator="\n\n", chunk_size=CHUNK_SIZE, chunk_overlap=CHUNK_OVERLAP)
+    text_splitter = CharacterTextSplitter(
+        separator="\n\n", chunk_size=CHUNK_SIZE, chunk_overlap=CHUNK_OVERLAP)
     return text_splitter.split_documents(documents)
 
 
@@ -26,10 +30,11 @@ def tag_documents(docs, department):
     return taggedDocs
 
 
+
 def insert_data_to_vector_store(docs):
     qdrant = Qdrant.from_documents(
         docs,
-        embeddings,
+        OpenAIEmbeddings(),
         url=QDRANT_URL,
         prefer_grpc=True,
         api_key=QDRANT_API_KEY,
@@ -43,5 +48,5 @@ def perform_data_insertion(department, file_name):
     insert_data_to_vector_store(taggedDocs)
 
 
-perform_data_insertion("history", "history_dept_data.txt")
-# perform_data_insertion("science", "science_dept_data.txt")
+perform_data_insertion(HISTORY_DEPARTMENT_NAME, "history_dept_data.txt")
+perform_data_insertion(SCIENCE_DEPARTMENT_NAME, "science_dept_data.txt")
